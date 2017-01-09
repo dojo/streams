33e354699464ecca14fd37f895441e02355859e3
Rory Mulligan <rorticus@gmail.com>
2017-01-09 12:48:47 -0500

    Adding pipeToStream method for streaming HTTP responses, issue #core-252

---
+++ src/pipeToStream.ts
@@ -0,0 +1,22 @@
+import { Response, DataEvent } from 'dojo-core/request';
+import { NodeResponse } from 'dojo-core/request/providers/node';
+import WritableStream from './WritableStream';
+import Promise from 'dojo-shim/Promise';
+
+export default function pipeToStream<T>(response: Response, stream: WritableStream<T>): Promise<WritableStream<T>> {
+	return new Promise<WritableStream<T>>((resolve) => {
+		response.on('data', (event: DataEvent) => {
+			stream.write(event.chunk);
+		});
+
+		response.on('end', () => {
+			stream.close();
+			resolve(stream);
+		});
+
+		// don't want to save the body because the file could be huge
+		if (response instanceof NodeResponse) {
+			response.downloadBody = false;
+		}
+	});
+}
---
+++ tests/unit/pipeToStream.ts
@@ -0,0 +1,91 @@
+import * as assert from 'intern/chai!assert';
+import * as registerSuite from 'intern!object';
+import pipeToStream from '../../src/pipeToStream';
+import { Headers, Response } from 'dojo-core/request';
+import { queueTask } from 'dojo-core/queue';
+import Task from 'dojo-core/async/Task';
+import ArraySink from '../../src/ArraySink';
+import WritableStream from '../../src/WritableStream';
+
+class MockResponse extends Response {
+	data: string[];
+
+	constructor(data: string[]) {
+		super();
+
+		this.data = data;
+
+		queueTask(() => {
+			data.forEach(chunk => {
+				this.emit({
+					type: 'data',
+					response: this,
+					chunk
+				});
+			});
+
+			this.emit({
+				type: 'end',
+				response: this
+			});
+		});
+	}
+
+	get bodyUsed(): boolean {
+		return false;
+	}
+
+	get headers(): Headers {
+		return new Headers();
+	}
+
+	get ok(): boolean {
+		return true;
+	}
+
+	get status(): number {
+		return 200;
+	}
+
+	get statusText(): string {
+		return 'OK';
+	}
+
+	get url(): string {
+		return '';
+	}
+
+	arrayBuffer(): Task<ArrayBuffer> {
+		return <any> null;
+	}
+
+	blob(): Task<Blob> {
+		return <any> null;
+	}
+
+	formData(): Task<FormData> {
+		return <any> null;
+	}
+
+	text(): Task<string> {
+		return <any> null;
+	}
+}
+
+registerSuite({
+	name: 'pipeToStream',
+
+	'sends data to stream'() {
+		const response = new MockResponse([
+			'bit 1',
+			'bit 2'
+		]);
+
+		const sink = new ArraySink<string>();
+		const stream = new WritableStream<string>(sink);
+
+		return pipeToStream(response, stream).then(() => {
+			assert.deepEqual(sink.chunks, [ 'bit 1', 'bit 2' ]);
+		});
+	}
+});
